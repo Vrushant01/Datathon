0320dbe83171fb15165c2ee97df37bb163816c45
Vrushant01 <shiroyavrushant07@gmail.com>
2026-08-25 21:27:12 +0530

    fix(deploy): untrack dist/ and add .catalystignore to ensure compiled output is deployed to AppSail

--- backend/dist/ai/tools/schemaExplorer.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.schemaExplorer = void 0;
-const schemaCache_1 = require("../schemaCache");
-const schemaExplorer = async () => {
-    return await (0, schemaCache_1.getSchema)();
-};
-exports.schemaExplorer = schemaExplorer;
