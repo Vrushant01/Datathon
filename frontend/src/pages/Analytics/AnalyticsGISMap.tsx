382e9174b161348728718e0cf087d870f66b106b
Vrushant01 <shiroyavrushant07@gmail.com>
2026-08-26 17:01:51 +0530

    mappifix done

--- frontend/src/pages/Analytics/AnalyticsGISMap.tsx
+++ frontend/src/pages/Analytics/AnalyticsGISMap.tsx
@@ -5,6 +5,9 @@ import { useAuth } from '../../context/AuthContext';
 import { MapPin, Filter } from 'lucide-react';
 import * as maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import workerUrl from 'maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url';
+
+maplibregl.setWorkerUrl(workerUrl);
 import { getMappedGeoJsonFeature, getBoundingBox, createCirclePolygon } from '../../utils/geoUtils';
 import karnatakaGeoJsonUrl from '../../assets/karnataka_districts.geojson?url';
 
