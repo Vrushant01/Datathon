a023f90f2c1f41ccefaea833dcbe0ecef273e38e
Vrushant01 <shiroyavrushant07@gmail.com>
2026-08-26 17:01:51 +0530

    mappifix done

--- frontend/src/pages/Admin/AdminGISMap.tsx
+++ frontend/src/pages/Admin/AdminGISMap.tsx
@@ -3,6 +3,9 @@ import { useLocation } from 'react-router-dom';
 import { mockDb } from '../../utils/mockDb';
 import * as maplibregl from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
+import workerUrl from 'maplibre-gl/dist/maplibre-gl-worker.mjs?worker&url';
+
+maplibregl.setWorkerUrl(workerUrl);
 import { Filter, Layers, Info } from 'lucide-react';
 import { getMappedGeoJsonFeature, getBoundingBox, createCirclePolygon } from '../../utils/geoUtils';
 import { API_BASE_URL } from '../../config/api';
